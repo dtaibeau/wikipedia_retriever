e562d28f964507ee224d3cb6284e62564c5f33b5
Delphine Tai-Beauchamp <delphinetaibeauchamp@gmail.com>
2024-07-17 18:32:08 -0700

    fetch using requests

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 from fastapi import FastAPI
+import requests
+# from pydantic import BaseModel
 
 app = FastAPI()
+
+# class FirstParagraph(BaseModel):
+#     """Data model for paragraph segment"""
+#     title: str
+#     text: str
+
 
 @app.get("/")
 async def read_root():
@@ -9,4 +17,14 @@ async def read_root():
 
 @app.get("/article/{article_name}")
 async def get_article(article_name: str):
-    return {"article_name": article_name}
+    url = f'https://en.wikipedia.org/api/rest_v1/page/summary/{article_name}'
+    response = requests.get(url)
+    if response.status_code == 200:
+        data = response.json()
+        return {
+            "title": data.get("title"),
+            "extract": data.get("extract")
+        }
+    else:
+        return {"error": "Article not found"}
+
